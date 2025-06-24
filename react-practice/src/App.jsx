1ca97b383004de8c0ab2b66c84ef9b21b2c16257
yoojanghun <yoojanghun@naver.com>
2025-06-25 00:15:55 +0900

    prop

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,16 +1,24 @@
-// How to STYLE REACT COMPONENTS with CSS
-// --------------------------------------
-// (not including external frameworks or preprocessors)
+// props = read-only properties that are shared between components.
+//         A parent component can send data to a child component.
+//         <Component key=value />
 
-// 1. EXTERNAL: index.css에 직접 CSS 작성 => 프로젝트 커지면 class이름이 많아 헷갈림
-// 2. MODULES: 하나의 component를 위한 폴더 만들고 안에 해당 jsx, CSS 파일을 담음, 그리고 css파일에 jsx 파일 연결 => Global style이 적용 안될 수 있음
-// 3. INLINE: component의 jsx파일 내부에 style 객체 만듦, return 요소에 style = {styles} 작성 => style이 적게 필요한 요소에 사용, 가독성 낮아짐, 반응형 CSS 등 복잡한 CSS에서 불리
 
-import Button from "./button.jsx"
+// propTypes = a mechanism that ensures that the passed value
+//             is of the correct datatype.
+//             age: PropTypes.number
+
+
+// 아래처럼 name 속성을 설정하면 Student.jsx에서 props 변수에 name(SpongeBob)이 저장됨
+import Student from "./Student.jsx";
 
 function App() {
   return(
-    <Button></Button>
+    <>
+      <Student name="SpongeBob" age={30} isStudent={true}></Student>
+      <Student name="Patric" age={42} isStudent={false}></Student>
+      <Student name="SquidWard" age={50} isStudent={false}></Student>
+      <Student name="Sandy" age={27} isStudent={true}></Student>
+    </>
   );
 }
 
